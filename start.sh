c9f0ce51d22f33498e16ae5969ad71d60df9808e
thebetter44 <deepspixel4@gmail.com>
2026-04-12 15:13:10 +0530

    Update comments and download links in start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# 1. Download Playit.gg (Direct Binary Link)
+# 1. Download Playit.gg (Direct Raw Link)
 echo "Setting up Playit.gg..."
 rm -f playit
 curl -L -o playit https://github.com
 chmod +x playit
 ./playit --secret $PLAYIT_SECRET &
 
-# 2. Download Fabric Server (Keep this, it's working!)
+# 2. Download Fabric Server (Direct Meta Link)
 echo "Downloading Fabric Server..."
-if [ ! -f fabric-server-launch.jar ]; then
-  curl -L -o fabric-server-launch.jar https://fabricmc.net
-fi
+rm -f fabric-server-launch.jar
+curl -L -o fabric-server-launch.jar https://fabricmc.net
 
 # 3. Accept EULA
 echo "eula=true" > eula.txt
 
-# 4. Start Minecraft
+# 4. Launch Minecraft
 echo "Launching Paradise Minecraft..."
 java -Xms8G -Xmx12G -jar fabric-server-launch.jar nogui
