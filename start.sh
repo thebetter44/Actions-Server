91b09eab890c4ba406e7f5b6326f9ccec5f79f4d
thebetter44 <deepspixel4@gmail.com>
2026-04-12 14:52:46 +0530

    Replace wget with curl for downloads in start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# 1. Download the correct Playit.gg Agent
+# 1. Download Playit.gg (Direct Binary)
 echo "Setting up Playit.gg..."
-wget -O playit https://github.com
+curl -L -o playit https://github.com
 chmod +x playit
 ./playit --secret $PLAYIT_SECRET &
 
-# 2. Download the actual Fabric Server JAR
+# 2. Download Fabric Server (Direct Meta Link)
 echo "Downloading Fabric Server..."
 rm -f fabric-server-launch.jar
-wget -O fabric-server-launch.jar https://fabricmc.net
+curl -L -o fabric-server-launch.jar https://fabricmc.net
 
 # 3. Accept EULA
 echo "eula=true" > eula.txt
